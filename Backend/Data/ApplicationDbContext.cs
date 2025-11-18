09422f4950829b0a3db2ef511fb3dec497d1a921
Mikael Gille <mikael.gille@gmail.com>
2025-11-18 14:06:58 +0100

    Seedat kategorier till db. Testat att lägga till produkter med SQL query

--- Backend/Data/ApplicationDbContext.cs
+++ Backend/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -11,4 +12,15 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public DbSet<Product> Products { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Transaction> Transactions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Category>().HasData(
+            new Category { CategoryId = 1, CategoryName = "Kategori1" },
+            new Category { CategoryId = 2, CategoryName = "Kategori2" },
+            new Category { CategoryId = 3, CategoryName = "Kategori3" }
+        );
+    }
 }
