fa89488c403800d1aa67872b9c9757bb0f002e3e
Emilia Almgren <emiliaalmgren@hotmail.com>
2025-11-19 10:28:15 +0100

    - Påbörjan på styleing för login-sidan
    - Lagt till lite exempel-färger i :root

--- Frontend/src/pages/LoginPage.tsx
+++ Frontend/src/pages/LoginPage.tsx
@@ -1,4 +1,5 @@
 import { useAuth } from "../context/AuthContext"
+import styles from "../styles/LoginPage.module.css"
 
 export default function LoginPage() {
   const { login } = useAuth();
@@ -9,22 +10,22 @@ export default function LoginPage() {
 
   return (
     <div>
-      <form action="">
+      <form action="" className={styles ["form-wrapper"]}>
         <h2>Logga in</h2>
         <fieldset>
-          <div>
+          <div className={styles ["form-colum"]}>
             <label htmlFor="name">Användarnamn </label>
             <input type="email" placeholder="exempel@hotmail.com" required/>
           </div>
 
-          <div>
+          <div className={styles ["form-colum"]}>
             <label htmlFor="password">Lösenord </label>
             <input type="password" required/>
           </div>
 
           <a href="#">Har du glömt ditt användarnamn eller lösenord?</a>
 
-          <div>
+          <div className={styles ["form-button"]}>
             <button onClick={handleLogin}>Logga in</button>
           </div>
 
